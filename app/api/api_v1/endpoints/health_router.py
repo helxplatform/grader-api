861d964a727a5a3b258099d56d5cc071082339f6
Hoid <tylerlcheek@gmail.com>
2024-11-06 14:38:02 -0500

    Snake case in the health_router

--- app/api/api_v1/endpoints/health_router.py
+++ app/api/api_v1/endpoints/health_router.py
@@ -4,10 +4,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/health/live", response_model=None, status_code=200)
-async def heathcheckLive():
+async def healthcheck_live():
   pass
 
 @router.get("/health/ready", response_model=None, status_code=200)
-async def heathcheckReady():
+async def healthcheck_ready():
   # In the future, check the health of redis and other internal systems here
   pass
